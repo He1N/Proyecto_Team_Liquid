4fc90a6ae3753e020368a71c9ae88c8b8e2006ba
He1N <crisenrique110@gmail.com>
2023-05-13 21:55:25 -0500

    Background del GUI

---
+++ gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+#ventana principal
+root = tk.Tk()
+root.configure(bg="#6cc3d2")
+
+#tamaño de pantalla
+root.geometry("1920x1080")
+
+# Lanzamos el loop principal de la ventana
+root.mainloop()
