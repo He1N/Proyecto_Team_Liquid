9ea6284f29467a21a04fffffba33587a04edb073
Cristian Enrique Arredondo Vargas <110081529+He1N@users.noreply.github.com>
2023-05-14 21:22:39 -0500

    Agregando Titulo al GUI con fuente personalizada

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 #ventana principal
 root = tk.Tk()
@@ -6,6 +7,23 @@ root.configure(bg="#6cc3d2")
 
 #tamaño de pantalla
 root.geometry("1920x1080")
+
+#Titulo
+title = tk.Label(
+    root,
+    text="PROGRAMA DIGITAL DE REGISTRO DE NOTAS",
+    background="#6cd2d1",
+    relief="solid",
+    borderwidth=3,
+    font=("e Explosive Attack", 36, "bold"	),
+    fg="#184e4e",
+    width="40",
+    height="2",
+    pady="10"
+)
+
+title.place(x=50, y=50)
+title.pack()
 
 # Lanzamos el loop principal de la ventana
 root.mainloop()
