f5c93dd7e467664c1e5b1f0ff05a79355b10dc1b
Cristian Enrique Arredondo Vargas <110081529+He1N@users.noreply.github.com>
2023-05-15 23:02:32 -0500

    Agregado de frames en GUI

--- gui.py
+++ gui.py
@@ -21,9 +21,25 @@ title = tk.Label(
     height="2",
     pady="10"
 )
-
+#posicion del title
 title.place(x=50, y=50)
 title.pack()
+
+for i in range(0,5):
+
+# Crea un Frame en el lado izquierdo de la ventana
+    frame = tk.Frame(root, bg="black", width=300, height=500,borderwidth=3)
+# Crea un canvas dentro del Frame
+    canvas = tk.Canvas(
+    frame, 
+    bg='#6cd2d1', 
+    width=280, 
+    height=500, 
+    bd=0, 
+    highlightthickness=0,
+    )
+    frame.place(x=(i*300)+20, y=200)
+    canvas.pack()
 
 # Lanzamos el loop principal de la ventana
 root.mainloop()
