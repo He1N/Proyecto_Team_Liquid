c148fa56c0c13d7d887797a915fd07a17a9cd4c9
Cristian Enrique Arredondo Vargas <110081529+He1N@users.noreply.github.com>
2023-05-16 18:15:50 -0500

    Finalización de GUI v1.

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 #ventana principal
 root = tk.Tk()
@@ -22,8 +23,8 @@ title = tk.Label(
     pady="10"
 )
 #posicion del title
-title.place(x=50, y=50)
-title.pack()
+title.place(x=170, y=40)
+
 
 for i in range(0,5):
 
@@ -58,6 +59,93 @@ reporte1 = tk.Label(text="REPORTE DE",font=("arial",15,"bold"),bg="#6cd2d1")
 reporte1.place(x=1290,y=230)
 reporte2= tk.Label(text="ESTUDIANTES",font=("arial",15,"bold"),bg="#6cd2d1")
 reporte2.place(x=1290,y=260)
+
+#Implementando imagenes
+#IMAGEN 1
+# Cargar la imagen
+image1 = Image.open("E:/Universidad/Compi/icons/agrega.png")
+image1 = image1.resize((200, 200))
+photo1 = ImageTk.PhotoImage(image1)
+
+# Crear una etiqueta para mostrar la imagen
+image_label1 = tk.Label(root, image=photo1,bg="#6cd2d1")
+image_label1.place(x=65,y=310)
+
+#IMAGEN 2
+image2 = Image.open("E:/Universidad/Compi/icons/editar.png")
+image2 = image2.resize((200, 200))
+photo2 = ImageTk.PhotoImage(image2)
+
+image_label2 = tk.Label(root, image=photo2,bg="#6cd2d1")
+image_label2.place(x=365,y=310)
+
+#IMAGEN 3
+image3 = Image.open("E:/Universidad/Compi/icons/eliminar.png")
+image3 = image3.resize((200, 200))
+photo3 = ImageTk.PhotoImage(image3)
+
+image_label3 = tk.Label(root, image=photo3,bg="#6cd2d1")
+image_label3.place(x=665,y=310)
+
+#IMAGEN 4
+image4 = Image.open("E:/Universidad/Compi/icons/buscar.png")
+image4 = image4.resize((200, 200))
+photo4 = ImageTk.PhotoImage(image4)
+
+image_label4 = tk.Label(root, image=photo4,bg="#6cd2d1")
+image_label4.place(x=965,y=310)
+
+#IMAGEN 5
+image5 = Image.open("E:/Universidad/Compi/icons/reporte.png")
+image5 = image5.resize((200, 200))
+photo5 = ImageTk.PhotoImage(image5)
+
+image_label5 = tk.Label(root, image=photo5,bg="#6cd2d1")
+image_label5.place(x=1265,y=310)
+
+#MINIDESCRIPCION
+#TEXTO1
+texto1_p1 = tk.Label(text="Permite crear nuevos",font=("arial",14),bg="#6cd2d1")
+texto1_p1.place(x=75,y=540)
+texto1_p2 = tk.Label(text="registros de estudiantes.",font=("arial",14),bg="#6cd2d1")
+texto1_p2.place(x=65,y=570)
+#TEXTO2
+texto2_p1 = tk.Label(text="Permite editar información",font=("arial",14),bg="#6cd2d1")
+texto2_p1.place(x=350,y=540)
+texto2_p2 = tk.Label(text="del registro de estudiantes.",font=("arial",14),bg="#6cd2d1")
+texto2_p2.place(x=350,y=570)
+#TEXTO3
+texto3_p1 = tk.Label(text="Permite eliminar información",font=("arial",14),bg="#6cd2d1")
+texto3_p1.place(x=640,y=540)
+texto3_p2 = tk.Label(text="del registro de estudiantes.",font=("arial",14),bg="#6cd2d1")
+texto3_p2.place(x=650,y=570)
+#TEXTO4
+texto4_p1 = tk.Label(text="Permite buscar estudiantes",font=("arial",14),bg="#6cd2d1")
+texto4_p1.place(x=950,y=540)
+texto4_p2 = tk.Label(text="del registro.",font=("arial",14),bg="#6cd2d1")
+texto4_p2.place(x=1010,y=570)
+#TEXTO5
+texto4_p1 = tk.Label(text="Permite generar un",font=("arial",14),bg="#6cd2d1")
+texto4_p1.place(x=1290,y=540)
+texto4_p2 = tk.Label(text="reporte de estudiantes.",font=("arial",14),bg="#6cd2d1")
+texto4_p2.place(x=1270,y=570)
+
+#BOTONES DE ACCION
+#BOTON 1
+boton1 = tk.Button(root, text="AGREGAR",font=("arial",15,"bold"))
+boton1.place(x=110,y=620)
+#BOTON 2
+boton1 = tk.Button(root, text="EDITAR",font=("arial",15,"bold"))
+boton1.place(x=420,y=620)
+#BOTON 3
+boton1 = tk.Button(root, text="ELIMINAR",font=("arial",15,"bold"))
+boton1.place(x=720,y=620)
+#BOTON 4
+boton1 = tk.Button(root, text="BUSCAR",font=("arial",15,"bold"))
+boton1.place(x=1020,y=620)
+#BOTON 5
+boton1 = tk.Button(root, text="GENERAR",font=("arial",15,"bold"))
+boton1.place(x=1320,y=620)
 
 # Lanzamos el loop principal de la ventana
 root.mainloop()
